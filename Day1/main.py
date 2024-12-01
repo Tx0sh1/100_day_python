3bf515c3d27cf19afa8e64865d1c0337bad9c416
Tx0sh1 <clydemojura@protonmail.com>
2024-12-01 10:46:04 +0200

    Day1

---
+++ Day1/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in? ")
+pet = input("what's your pet's name? ")
+band_name = city + " " + pet
+
+print(band_name)
